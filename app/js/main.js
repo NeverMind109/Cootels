99e96b233d87901db2ffee356164bf015c1ce9e7
NeverMind109 <vladgladkih123@gmail.com>
2022-02-18 20:01:00 +0300

    Created: Added Questions Section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
-    
-})
+  $(".questions__accordion-title").on("click", function () {
+    $(this).toggleClass("accordion--active").next().slideToggle();
+    $(".questions__accordion-title")
+      .not(this)
+      .removeClass("accordion--active")
+      .next()
+      .slideUp();
+  });
+  $(".questions__accordion-open")
+    .addClass("accordion--active")
+    .next()
+    .slideToggle();
+});
